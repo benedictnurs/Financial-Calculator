405c81c52fd59592d32b199b5d85bda6983cce9c
benedictnurs <benedictnursalim@gmail.com>
2022-11-14 00:07:05 -0800

    Calculations
    -Added a chart and a description of prices.

--- Welcome.py
+++ Welcome.py
@@ -17,21 +17,3 @@ def sidebar():
   with st.sidebar:
     st.sidebar.success("Select Page Above")
 sidebar()
-
-df = pd.DataFrame(
-    np.random.randn(10, 5),
-    columns=("col %d" % i for i in range(5)))
-
-# CSS to inject contained in a string
-hide_table_row_index = """
-            <style>
-            thead tr th:first-child {display:none}
-            tbody th {display:none}
-            </style>
-            """
-
-# Inject CSS with Markdown
-st.markdown(hide_table_row_index, unsafe_allow_html=True)
-
-# Display a static table
-st.table(df)
--- pages/1_Car_Loan.py
+++ pages/1_Car_Loan.py
@@ -7,7 +7,7 @@ import altair as alt
 #streamlit run main.py
 def configPage():
   st.set_page_config(
-    page_title = "Car Loan",
+    page_title = "Auto Loan Calculator",
     page_icon="📈",
     initial_sidebar_state="collapsed"
 )
@@ -20,13 +20,15 @@ sidebar()
 
 #Get the user input values
 def values():
-    global price, down, interest, tax, monthly
+    global price, down, interest, tax, monthly, topCols
     st.title("Car Loan Calculator")
-    price = st.number_input("Car Total")
-    down = st.number_input("Down Payment")
-    interest = st.number_input("Interest Rate")
-    tax = st.number_input("Sales Tax")
-    monthly = st.selectbox(
+    topCols = st.columns(2)
+    with topCols[0]:
+        price = st.number_input("Car Total")
+        down = st.number_input("Down Payment")
+        interest = st.number_input("Interest Rate")
+        tax = st.number_input("Sales Tax")
+        monthly = st.selectbox(
     'How many months will the loan be?',
     ('12', '24', '48','60','72','84'))
 values()
@@ -34,6 +36,7 @@ values()
 taxRate = price * tax/100
 total = (taxRate + price)  
 rate = (interest / 100)/12
+
 
 def years():
     global month, year
@@ -53,7 +56,7 @@ def years():
 years()
 
 def selection():
-    global monthlyPayment
+    global monthlyPayment, upfront
     agree = st.checkbox('Include All Fees in Loan')
     if agree:
         if rate == 0 :
@@ -64,28 +67,65 @@ def selection():
         if rate == 0 :
             monthlyPayment =  (total - down)/month 
         else:
-            monthlyPayment = (price - down) * (rate*(1+rate)**month)/((1+rate)**month-1)    
+            monthlyPayment = (price - down) * (rate*(1+rate)**month)/((1+rate)**month-1) 
+    if agree:
+        upfront = taxRate + down
+    else:
+        upfront = taxRate + down
 selection()
 
-st.subheader("Monthly Payment: $" + (str("{:,}".format(round(float(monthlyPayment), 2)))))
+
+
+interestPaid = round((total - (monthlyPayment * month)),2)
+
+
+
+source = pd.DataFrame({"Loan Breakdown": ["Principal", "Interest"], "Dollars": [total, interestPaid]})
+pie = alt.Chart(source).mark_arc().encode(
+        theta=alt.Theta(field="Dollars", type="quantitative"),
+        color=alt.Color(field="Loan Breakdown", type="nominal"),    
+        tooltip=['Loan Breakdown', alt.Tooltip('Dollars:Q',  format="$,.2f")]
+    ).interactive()
+
+
+with topCols[1]:
+    html_str = f"""
+    <style>
+    p.a {{
+    font: 20vw sans-serif;
+    }}
+    </style>
+    <p></p>
+    <h3 class="a">Monthly Pay: $ {(str("{:,}".format(round(float(monthlyPayment), 2))))}</h3>
+    <h3 class="a">Sale Tax: $ {(str("{:,}".format(round(float(taxRate), 2))))}</h3>
+    <h3 class="a">Upfront: $ {(str("{:,}".format(round(float(upfront), 2))))}</h3>
+    <h3 class="a">Total Interest: $ {(str("{:,}".format(round(float(interestPaid), 2))))}</h3>
+    <h3 class="a">Total Cost: $ {(str("{:,}".format(round(float(monthlyPayment), 2))))}</h3>
+    """
+    st.markdown(html_str, unsafe_allow_html=True)
+
 
 def tabs():
-  tab1, tab2 , tab3 = st.tabs(["Chart","Data", "Table",])
+  tabs = st.tabs(["Chart", "Table",])
   #Chart
-  tab1.subheader('Increase Per Year Chart')
+  tab_chart = tabs[0]
+  tab_data = tabs[1]
+  
+  with tab_chart:
+        cols = st.columns(2)
+  with cols[0]:
+        st.subheader("Loan Breakdown")
+        st.altair_chart(pie, use_container_width=True)
+  with cols[1]:
+        st.subheader("Loan By Year")
+  
   #Data
-  tab2.subheader('Increase Per Year Data')
-  #Table
-  tab3.subheader('Increase Per Year Table')
+  with tab_data:
+        st.subheader('Data')
 tabs()
 
-interestPaid = (monthlyPayment * month) - total
-source = pd.DataFrame({"Loan Breakdown": ["Principal", "Interest"], "Dollars": [total, interestPaid]})
 
-plot = alt.Chart(source).mark_arc().encode(
-    theta=alt.Theta(field="Dollars", type="quantitative"),
-    color=alt.Color(field="Loan Breakdown", type="nominal"),    
-    tooltip=['Loan Breakdown', 'Dollars']
-).interactive()
 
-st.altair_chart(plot, use_container_width=False)
+
+
+
